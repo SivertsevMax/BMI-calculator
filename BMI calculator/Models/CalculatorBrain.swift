5610f8fd7ffec8e9b8d9d05827646e1075437e24
Sivertsev Maxim <sivertsev1995@icloud.com>
2023-02-21 17:38:53 +0300

    1

--- BMI calculator/Models/CalculatorBrain.swift
+++ BMI calculator/Models/CalculatorBrain.swift
@@ -8,15 +8,15 @@ struct CalculatorBrain {
         let BMIValue = weight / pow(height, 2)
         
         if BMIValue < 18.5 {
-            BMIResult = BMI(value: BMIValue, advice: "", color: .systemCyan)
+            BMIResult = BMI(value: BMIValue, advice: "Eat more", color: .systemCyan)
         } else if BMIValue < 24.9 {
-            BMIResult = BMI(value: BMIValue, advice: "Healthy", color: .systemGreen)
+            BMIResult = BMI(value: BMIValue, advice: "You're good", color: .systemGreen)
         } else if BMIValue < 30 {
-            BMIResult = BMI(value: BMIValue, advice: "Overweight", color: .systemYellow)
+            BMIResult = BMI(value: BMIValue, advice: "You need to control your eating", color: .systemYellow)
         } else if BMIValue < 40 {
-            BMIResult = BMI(value: BMIValue, advice: "Obese", color: .systemOrange)
+            BMIResult = BMI(value: BMIValue, advice: "You're crazy", color: .systemOrange)
         } else {
-            BMIResult = BMI(value: BMIValue, advice: "Extremely obese", color: .systemRed)
+            BMIResult = BMI(value: BMIValue, advice: "Please stop eating, you'll die", color: .systemRed)
         }
         print(BMIResult?.value ?? 0.0)
         
